c4d51332232ac570e488cef7a332583ba72fd800
Mo Mahboobian <momahboobian@gmail.com>
2023-09-12 23:34:17 +0100

    Custom btn component

---
+++ components/reusable/CustomButton.jsx
@@ -0,0 +1,7 @@
+export default function CustomButton({ title }) {
+  return (
+    <button className="font-semibold text-secondary-dark items-center rounded-lg bg-orange-400 px-4 py-2">
+      {title}
+    </button>
+  );
+}
