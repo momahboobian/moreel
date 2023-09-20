d2adcc51901a92f9c154683a69137d3f2a3c0db9
Mo Mahboobian <momahboobian@gmail.com>
2023-09-20 20:31:06 +0100

    Adding profile image & g push

--- components/shared/Sidebar.jsx
+++ components/shared/Sidebar.jsx
@@ -1,11 +1,24 @@
+import Image from "next/image";
+import bannerImage from "../../public/images/MoReel.jpg";
+
 export default function Sidebar() {
   return (
-    <section className="flex flex-col sm:justify-between items-center sm:flex-row mt-5 md:mt-2 bg-secondary-dark text-white rounded-2xl">
-      <div className="w-full md:w-1/3 text-left p-4 ">
-        <h2 className="text-2xl font-bold whitespace-pre  ">Mo Mahboobian</h2>
-        <p className="font-normal text-base text-gray-500 py-6">
-          <span className=" whitespace-pre py-6">Software Developer &</span>
-          <span className="whitespace-pre py-6"> Video Engineer</span>
+    <section className="flex flex-col sm:justify-between items-center p-4 mt-5 md:mt-2 bg-secondary-dark text-white rounded-2xl">
+      <div className="relative w-32 h-32 overflow-hidden rounded-full">
+        <Image
+          layout="fill"
+          objectFit="cover"
+          src={bannerImage}
+          alt="Profile image"
+        />
+      </div>
+      <div className="flex flex-col items-center w-full text-left ">
+        <h2 className="text-2xl font-bold whitespace-pre mt-4">
+          Mo Mahboobian
+        </h2>
+        <p className="flex flex-col items-center mt-4 font-normal text-base text-gray-500 ">
+          <span className="">Software Developer</span>{" "}
+          <span className="whitespace-pre ">& Video Engineer</span>
         </p>
       </div>
     </section>
