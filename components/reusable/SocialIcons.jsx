7219cb180076886286c8411796b928866f3d964d
Mo Mahboobian <momahboobian@gmail.com>
2023-09-20 21:16:17 +0100

    Adding Languages

---
+++ components/reusable/SocialIcons.jsx
@@ -0,0 +1,22 @@
+import { FaLinkedin, FaTwitter, FaGlobe, FaInstagram } from "react-icons/fa";
+
+export default function SocialIcons({ socialLinks }) {
+  return (
+    <div className="flex space-x-4">
+      {socialLinks.map((link, index) => (
+        <a
+          key={index}
+          href={link.url}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-orange-400 transition duration-300 hover:text-orange-600 hover:opacity-40"
+        >
+          {link.type === "linkedin" && <FaLinkedin size={24} />}
+          {link.type === "twitter" && <FaTwitter size={24} />}
+          {link.type === "instagram" && <FaInstagram size={24} />}
+          {link.type === "website" && <FaGlobe size={24} />}
+        </a>
+      ))}
+    </div>
+  );
+}
--- components/shared/Sidebar.jsx
+++ components/shared/Sidebar.jsx
@@ -17,7 +17,7 @@ const socialLinks = [
 
 export default function Sidebar() {
   return (
-    <section className="flex flex-col sm:justify-between items-center p-10 mt-5 space-y-4 md:mt-2 bg-secondary-dark text-white rounded-2xl">
+    <section className="flex flex-col sm:justify-between items-center p-6 mt-5 space-y-6 md:mt-2 bg-secondary-dark text-white rounded-2xl">
       <div className="relative w-32 h-32 overflow-hidden rounded-full">
         <Image
           layout="fill"
@@ -26,9 +26,9 @@ export default function Sidebar() {
           alt="Profile image"
         />
       </div>
-      <div className="flex flex-col items-center w-full text-left ">
-        <h2 className="text-2xl whitespace-pre ">Mo Mahboobian</h2>
-        <p className="flex flex-col items-center mt-4 font-normal text-base text-gray-500 ">
+      <div className="flex flex-col items-center w-full text-left">
+        <h2 className="text-2xl whitespace-pre">Mo Mahboobian</h2>
+        <p className="flex flex-col items-center mt-4 font-normal text-base text-gray-500">
           <span className="">Software Developer</span>{" "}
           <span className="whitespace-pre ">& Video Engineer</span>
         </p>
@@ -36,8 +36,39 @@ export default function Sidebar() {
       <div className="flex gap-2">
         <SocialIcons socialLinks={socialLinks} />
       </div>
-      <hr className="my-4 w-full border-gray-300 opacity-40" />{" "}
       {/* Line separator */}
+      <hr className="my-4 w-full border-gray-300 opacity-80" />
+
+      <div className="text-left text-white whitespace-pre">
+        <p className="flex justify-between text-base gap-8">
+          <span>Email:</span>{" "}
+          <a href="mailto:info@moreel.me" className="text-right">
+            info@moreel.me
+          </a>
+        </p>
+        <p className="flex justify-between text-base">
+          <span>Location:</span> London, UK
+        </p>
+      </div>
+
+      {/* Line separator */}
+      <hr className="my-6 w-full border-gray-300 opacity-80" />
+      <div className="w-full justify-start text-left space-y-2">
+        <h3 className="text-xl">Languages</h3>
+        <p className="font-normal  text-gray-500">English</p>
+        <hr className="w-full border-orange-400 border-t-4 rounded-md" />
+        <p className="pt-2 font-normal  text-gray-500">Persian</p>
+        <hr className="w-full border-orange-400 border-t-4 rounded-md" />
+      </div>
+
+      {/* Line separator */}
+      <hr className=" my-6 w-full border-gray-300 opacity-80" />
+      <div className="w-full justify-start text-left space-y-2">
+        <h3 className="text-xl">Skills</h3>
+      </div>
+
+      {/* Line separator */}
+      <hr className=" my-6 w-full border-gray-300 opacity-80" />
     </section>
   );
 }
