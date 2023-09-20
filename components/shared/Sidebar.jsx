fa250e925df495f067bdc253cd2d6714ebb92bdc
Mo Mahboobian <momahboobian@gmail.com>
2023-09-20 20:46:36 +0100

    Adding social icons

--- components/shared/Sidebar.jsx
+++ components/shared/Sidebar.jsx
@@ -5,19 +5,19 @@ import SocialIcons from "../reusable/SocialIcons";
 const socialLinks = [
   {
     type: "linkedin",
-    url: "https://www.linkedin.com/in/your-linkedin-profile/",
+    url: "https://www.linkedin.com/in/mo-mahboobian/",
   },
-  { type: "twitter", url: "https://twitter.com/your-twitter-profile/" },
-  { type: "website", url: "https://www.your-website.com/" },
+  { type: "twitter", url: "https://twitter.com/momahboobian/" },
   {
     type: "instagram",
-    url: "https://www.instagram.com/your-instagram-profile/",
+    url: "https://www.instagram.com/mo.mahboobian/",
   },
+  { type: "website", url: "https://www.moreel.me/" },
 ];
 
 export default function Sidebar() {
   return (
-    <section className="flex flex-col sm:justify-between items-center p-4 mt-5 md:mt-2 bg-secondary-dark text-white rounded-2xl">
+    <section className="flex flex-col sm:justify-between items-center p-10 mt-5 space-y-4 md:mt-2 bg-secondary-dark text-white rounded-2xl">
       <div className="relative w-32 h-32 overflow-hidden rounded-full">
         <Image
           layout="fill"
@@ -27,9 +27,7 @@ export default function Sidebar() {
         />
       </div>
       <div className="flex flex-col items-center w-full text-left ">
-        <h2 className="text-2xl font-bold whitespace-pre mt-4">
-          Mo Mahboobian
-        </h2>
+        <h2 className="text-2xl whitespace-pre ">Mo Mahboobian</h2>
         <p className="flex flex-col items-center mt-4 font-normal text-base text-gray-500 ">
           <span className="">Software Developer</span>{" "}
           <span className="whitespace-pre ">& Video Engineer</span>
@@ -38,6 +36,8 @@ export default function Sidebar() {
       <div className="flex gap-2">
         <SocialIcons socialLinks={socialLinks} />
       </div>
+      <hr className="my-4 w-full border-gray-300 opacity-40" />{" "}
+      {/* Line separator */}
     </section>
   );
 }
