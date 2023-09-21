f449e18699400bb53838574dd8bfec376aa71002
Mo Mahboobian <momahboobian@gmail.com>
2023-09-21 21:47:52 +0100

    Adding Side Menu component

---
+++ components/shared/SideMenu.jsx
@@ -0,0 +1,7 @@
+export default function SideMenu() {
+  return (
+    <section className="flex flex-col sm:justify-between items-center mt-5 md:mr-3 md:mt-2 p-6 space-y-6 bg-secondary-dark text-white rounded-2xl">
+      <div className="">Side Menu</div>
+    </section>
+  );
+}
