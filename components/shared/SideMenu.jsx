f751e43bc92dd56a62823c337e27c2480ed78dfe
Mo Mahboobian <momahboobian@gmail.com>
2023-09-21 22:04:51 +0100

    Adding light/dark mode icon

--- components/shared/SideMenu.jsx
+++ components/shared/SideMenu.jsx
@@ -1,7 +1,9 @@
+import { GiMoon } from "react-icons/gi";
+
 export default function SideMenu() {
   return (
     <section className="flex flex-col sm:justify-between items-center mt-5 md:mr-3 md:mt-2 p-6 space-y-6 bg-secondary-dark text-white rounded-2xl">
-      <div className="">Side Menu</div>
+      <GiMoon size={24} className="text-orange-400 transform -rotate-45" />
     </section>
   );
 }
