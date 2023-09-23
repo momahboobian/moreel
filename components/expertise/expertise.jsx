1b3baa17c9b43d47e127d4856a1a1fafe79ec4f5
Mo Mahboobian <momahboobian@gmail.com>
2023-09-23 09:24:31 +0100

    Adding Expertise component

---
+++ components/expertise/expertise.jsx
@@ -0,0 +1,17 @@
+export default function Expertise() {
+  return (
+    <section className="flex flex-col sm:justify-between items-center mt-5 md:mt-2 sm:flex-row bg-secondary-dark text-white rounded-2xl">
+      <div className="w-full p-4 ">
+        <h1 className="text-5xl font-bold ">Expertise</h1>
+        <p className="font-normal text-base text-gray-500 py-6">
+          Cultivated a diverse skill set, gaining valuable insights across
+          various domains. I possess expertise in video and audio production,
+          along with a strong background in product design and software
+          development. This multidisciplinary knowledge equips me to approach
+          challenges with a holistic perspective, fostering innovation and
+          creative problem-solving."
+        </p>
+      </div>
+    </section>
+  );
+}
