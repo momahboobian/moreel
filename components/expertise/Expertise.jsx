d0fb0af0c73aa1724c30e9257171f2e2919ebe4c
Mo Mahboobian <momahboobian@gmail.com>
2023-10-03 10:32:33 +0100

    Adding portfolio component

--- components/expertise/Expertise.jsx
+++ components/expertise/Expertise.jsx
@@ -5,7 +5,7 @@ export default function Expertise() {
   return (
     <section className="flex flex-col justify-between mt-5 md:mt-2 text-white rounded-2xl">
       <div className="flex flex-col items-center w-full p-4 ">
-        <h1 className="text-5xl font-bold ">Expertise</h1>
+        <h2 className="text-5xl font-bold ">Expertise</h2>
         <p className="font-normal text-base text-gray-500 py-6">
           Experienced in software development and product design, I bring a
           versatile skill set that complements my proficiency in video and audio
---
+++ components/portfolio/Portfolio.jsx
@@ -0,0 +1,10 @@
+export default function Portfolio() {
+  return (
+    <section className="flex flex-col justify-between mt-5 md:mt-2 text-white rounded-2xl">
+      <div className="flex flex-col items-center w-full p-4 ">
+        <h2 className="text-5xl font-bold ">Portfolio</h2>
+        <p className="font-normal text-base text-gray-500 py-6">portfolio</p>
+      </div>
+    </section>
+  );
+}
