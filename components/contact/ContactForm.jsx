99a06e8c658c2e4a28bf1c52f06417bfac473a44
Mo Mahboobian <momahboobian@gmail.com>
2023-10-09 00:57:42 +0100

    Contact from size update

--- components/contact/ContactForm.jsx
+++ components/contact/ContactForm.jsx
@@ -25,13 +25,13 @@ export default function ContactForm() {
 
   return (
     <section className="grow text-white">
-      <div className="flex flex-col items-start text-start w-full p-4 ">
+      <div className="flex flex-col items-start text-start w-full p-4">
         <h5 className="text-3xl font-normal">Leave us your info</h5>
         <form onSubmit={handleSubmit} className="mt-4 w-full bg-white p-4">
           <div className="mb-4">
             <label
               htmlFor="fullName"
-              className="text-base font-normal text-gray-500"
+              className="text-base font-normal text-gray-500 "
             >
               Your Full Name <span className="text-orange-400">*</span>
             </label>
@@ -42,7 +42,7 @@ export default function ContactForm() {
               required
               value={formData.fullName}
               onChange={handleChange}
-              className="w-full px-3 py-2 text-white bg-transparent border-b border-gray-500 focus:outline-none focus:border-blue-500"
+              className="w-full px-3 py-3 text-white bg-transparent border-b border-gray-500 focus:outline-none focus:border-blue-500"
             />
           </div>
           <div className="mb-4">
@@ -59,7 +59,7 @@ export default function ContactForm() {
               required
               value={formData.email}
               onChange={handleChange}
-              className="w-full px-3 py-2 text-white bg-transparent border-b border-gray-500 focus:outline-none focus:border-blue-500"
+              className="w-full px-3 py-3 text-white bg-transparent border-b border-gray-500 focus:outline-none focus:border-blue-500"
             />
           </div>
           <div className="mb-4">
@@ -75,7 +75,7 @@ export default function ContactForm() {
               name="subject"
               value={formData.subject}
               onChange={handleChange}
-              className="w-full px-3 py-2 text-white bg-transparent border-b border-gray-500 focus:outline-none focus:border-blue-500"
+              className="w-full px-3 py-3 text-white bg-transparent border-b border-gray-500 focus:outline-none focus:border-blue-500"
             />
           </div>
           <div className="mb-4">
@@ -91,7 +91,7 @@ export default function ContactForm() {
               value={formData.message}
               onChange={handleChange}
               rows="4"
-              className="w-full px-3 py-2 text-white bg-transparent border-b border-gray-500 focus:outline-none focus:border-blue-500"
+              className="w-full px-3 py-9 text-white bg-transparent border-b border-gray-500 focus:outline-none focus:border-blue-500"
             ></textarea>
           </div>
 
