a89dbfc6297010d6701b23f954d18679191ff5b8
Mo Mahboobian <momahboobian@gmail.com>
2023-10-08 22:50:16 +0100

    last update on side nav bar

--- components/shared/SideCol.jsx
+++ components/shared/SideCol.jsx
@@ -38,14 +38,14 @@ export default function SideCol() {
   }, []);
   return (
     <section className="flex flex-col sm:justify-between items-center mt-5 md:ml-3  md:mt-2 p-6 space-y-6 bg-secondary-dark text-white rounded-2xl">
-      {/* <div className="relative w-32 h-32 overflow-hidden rounded-full">
+      <div className="relative w-32 h-32 overflow-hidden rounded-full">
         <Image
           layout="fill"
           objectFit="cover"
           src={bannerImage}
           alt="Profile image"
         />
-      </div> */}
+      </div>
       <div className="flex flex-col items-center w-full text-left">
         <h2 className="text-2xl whitespace-pre">Mo Mahboobian</h2>
         <p className="flex flex-col items-center mt-4 font-normal text-base text-gray-500">
--- components/shared/SideMenu.jsx
+++ components/shared/SideMenu.jsx
@@ -11,31 +11,53 @@ import {
 
 export default function SideMenu() {
   return (
-    <section className="flex flex-col sm:justify-between items-center mt-5 md:mr-3 md:mt-2 p-6 space-y-10 bg-secondary-dark text-white rounded-2xl group">
-      <GiMoon
-        size={28}
-        className="text-orange-400 mb-40 transform -rotate-45"
-      />
-      <div className="w-10 h-10 bg-orange-400 rounded-full flex items-center justify-center">
-        <BiSolidHomeAlt2
-          size={20}
-          className="text-gray-800 group-hover:text-white"
-        />
-      </div>
-      <div className="w-10 h-10 bg-white rounded-full flex items-center justify-center">
-        <FaFileCode size={20} className="text-gray-400" />
-      </div>
-      <div className="w-10 h-10 bg-white rounded-full flex items-center justify-center">
-        <MdWork size={20} className="text-gray-400" />
-      </div>
-      <div className="w-10 h-10 bg-white rounded-full flex items-center justify-center">
-        <BiSolidLeaf size={20} className="text-gray-400" />
-      </div>
-      <div className="w-10 h-10 bg-white rounded-full flex items-center justify-center">
-        <FaUserGraduate size={18} className="text-gray-400" />
-      </div>
-      <div className="w-10 h-10 bg-white rounded-full flex items-center justify-center">
-        <BiSolidMessageDetail size={20} className="text-gray-400" />
+    <section className="flex ">
+      <div className="flex flex-col sm:justify-between items-center mt-5 md:mr-3 md:mt-2 p-6 bg-secondary-dark text-white rounded-2xl ">
+        <div className="flex-none">
+          <GiMoon
+            size={28}
+            className="text-orange-400 mb-40 transform -rotate-45 hover:rotate-180 hover:text-white hover:cursor-pointer"
+          />
+        </div>
+        <div className="flex-none space-y-10 ">
+          <div className="w-10 h-10 bg-orange-400 rounded-full flex items-center justify-center group">
+            <BiSolidHomeAlt2
+              size={20}
+              className="text-gray-800 group-hover:text-white hover:cursor-pointer"
+            />
+          </div>
+          <div className="w-10 h-10 bg-white rounded-full flex items-center justify-center hover:bg-orange-400  hover:cursor-pointer group">
+            <FaFileCode
+              size={20}
+              className="text-gray-400 group-hover:text-gray-800"
+            />
+          </div>
+          <div className="w-10 h-10 bg-white rounded-full flex items-center justify-center hover:bg-orange-400  hover:cursor-pointer group">
+            <MdWork
+              size={20}
+              className="text-gray-400 group-hover:text-gray-800"
+            />
+          </div>
+          <div className="w-10 h-10 bg-white rounded-full flex items-center justify-center hover:bg-orange-400  hover:cursor-pointer group">
+            <BiSolidLeaf
+              size={20}
+              className="text-gray-400 group-hover:text-gray-800"
+            />
+          </div>
+          <div className="w-10 h-10 bg-white rounded-full flex items-center justify-center hover:bg-orange-400  hover:cursor-pointer group">
+            <FaUserGraduate
+              size={18}
+              className="text-gray-400 group-hover:text-gray-800"
+            />
+          </div>
+          <div className="w-10 h-10 bg-white rounded-full flex items-center justify-center hover:bg-orange-400  hover:cursor-pointer group">
+            <BiSolidMessageDetail
+              size={20}
+              className="text-gray-400 group-hover:text-gray-800"
+            />
+          </div>
+        </div>
+        <div className="grow h-96 mt-4 mb-36"> </div>
       </div>
     </section>
   );
