02b33eab32efbb78275fd0d84755a89d228c5b12
Mo Mahboobian <momahboobian@gmail.com>
2023-11-07 19:54:50 +0000

    Last update on Slills cloud

--- components/reusable/SkillsCloud.tsx
+++ components/reusable/SkillsCloud.tsx
@@ -1,50 +1,27 @@
-// import { useRecoilValue, useSetRecoilState } from "recoil";
-// import { shuffledSkillsSelector, skillsState } from "@/recoil/skillsState";
-// import skills from "@/data/skills";
-
-// export default function SkillsCloud() {
-//   const animatedSkills = useRecoilValue(shuffledSkillsSelector);
-//   const setSkillsState = useSetRecoilState(skillsState);
-
-//   // Initialize Recoil state with the skills array
-//   setSkillsState(skills);
-
-//   return (
-//     <div id="skills-container" className="skills-container">
-//       {animatedSkills?.map((skill: string, index: number) => (
-//         <p
-//           key={index}
-//           className="skill font-normal text-gray-500"
-//           style={{
-//             left: `${Math.random() * 100}%`, // Randomize initial left position
-//             top: `${Math.random() * 100}%`, // Randomize initial top position
-//             animation: `move-skill ${
-//               Math.floor(Math.random() * 2) + 4
-//             }s linear infinite`,
-//             animationDelay: `${index * 0.7}s`,
-//           }}
-//         >
-//           {skill}
-//         </p>
-//       ))}
-//     </div>
-//   );
-// }
-
 import { useRecoilValue } from "recoil";
-import { skillsState } from "@/recoil/skillsState";
+import { shuffledSkillsSelector } from "@/recoil/skillsState";
 
 export default function Skills() {
-  const skills = useRecoilValue(skillsState);
+  const animatedSkills = useRecoilValue(shuffledSkillsSelector);
 
   return (
-    <div className=" ">
-      <h3 className="text-xl pb-2">Skills</h3>
-      <ul>
-        {skills.map((skill, index) => (
-          <li key={index}>{skill}</li>
-        ))}
-      </ul>
+    <div id="skills-container" className="skills-container">
+      {animatedSkills?.map((skill: string, index: number) => (
+        <p
+          key={index}
+          className="skill font-normal text-gray-500"
+          style={{
+            left: `${Math.random() * 100}%`,
+            top: `${Math.random() * 100}%`,
+            animation: `move-skill ${
+              Math.floor(Math.random() * 2) + 4
+            }s linear infinite`,
+            animationDelay: `${index * 0.7}s`,
+          }}
+        >
+          {skill}
+        </p>
+      ))}
     </div>
   );
 }
--- recoil/skillsState.ts
+++ recoil/skillsState.ts
@@ -1,20 +1,38 @@
-import { atom } from "recoil";
+import { atom, selector } from "recoil";
+
+const initialSkills = [
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "React.js",
+  "Node.js",
+  "Tailwind CSS",
+  "Jest",
+  "GitHub",
+  "Python",
+  "Express.js",
+  "PostgreSQL",
+  "AWS",
+  "Next.js",
+];
 
 export const skillsState = atom<string[]>({
   key: "skillsState",
-  default: [
-    "HTML",
-    "CSS",
-    "JavaScript",
-    "React.js",
-    "Node.js",
-    "Tailwind CSS",
-    "Jest",
-    "GitHub",
-    "Python",
-    "Express.js",
-    "PostgreSQL",
-    "AWS",
-    "Next.js",
-  ],
+  default: initialSkills,
+});
+
+export const shuffledSkillsSelector = selector({
+  key: "shuffledSkillsSelector",
+  get: ({ get }) => {
+    const skills = get(skillsState);
+    const shuffledSkills = [...skills];
+    for (let i = shuffledSkills.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [shuffledSkills[i], shuffledSkills[j]] = [
+        shuffledSkills[j],
+        shuffledSkills[i],
+      ];
+    }
+    return shuffledSkills;
+  },
 });
