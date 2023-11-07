b8d4721f89778e3b2f35182d7b2405ac2a33e5f4
Mo Mahboobian <momahboobian@gmail.com>
2023-11-07 12:03:01 +0000

    Updating PortfolioCard Custom Image

--- reusable/PortfolioCard.tsx
+++ reusable/PortfolioCard.tsx
@@ -8,14 +8,7 @@ interface CustomPortfolioProps {
 export default function PortfolioCard({ image, alt }: CustomPortfolioProps) {
   return (
     <div className="relative max-w-full cursor-pointer duration-500 hover:transform hover:scale-105">
-      <Image
-        src={image}
-        alt={alt}
-        layout="responsive"
-        objectFit="cover"
-        width={400}
-        height={400}
-      />
+      <Image src={image} alt={alt} width={400} height={400} />
     </div>
   );
 }
