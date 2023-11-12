90b6a2a10b9daf0d1f82f46c086d9fd4b3e4fa71
Mo Mahboobian <momahboobian@gmail.com>
2023-11-12 15:49:48 +0000

    Updating the recomendation data

--- data/recommendationsData.js
+++ data/recommendationsData.js
@@ -4,7 +4,7 @@ const recommendationsData = [
     rating: 5,
     review: "Outstanding Colleague and Team Player",
     recommendation:
-      "Mo has an air of calm and maturity about him that inspires trust and made him a magnet for his colleagues during the ten month course. He was always willing to engage in tasks and contribute enthusiastically in group scenarios. He is bright, professional, thoughtful and a joy to work with - I would jump at the chance to work with him again.",
+      "“Mo has an air of calm and maturity about him that inspires trust and made him a magnet for his colleagues during the ten month course. He was always willing to engage in tasks and contribute enthusiastically in group scenarios. He is bright, professional, thoughtful and a joy to work with - I would jump at the chance to work with him again.“",
     avatar: "/images/rec-img/Jemima_Villanueva.jpeg",
     name: "Jemima Villanueva",
     title: "Executive Director",
@@ -13,32 +13,25 @@ const recommendationsData = [
     rating: 5,
     review: "Exceptional Developer and Collaborator",
     recommendation:
-      "Mo is an enthusiastic and talented developer with a broad range of skills across the stack, from building out aesthetic and functional designs in Figma to the implementation of tickets. He approaches tech with an attention to detail, both visually and in terms of understanding product requirements. He has brought a great deal of value to his peers on the course, as well as to team mates in projects. I had the pleasure of supervising his final team project, and the determination and infectious positive spirit that he brought to proceeding was a huge part in the projects success. I’d have no hesitation to recommend Mo to any product driven engineering team that would benefit from his unique combination of skills and experience.",
+      "“Mo is an enthusiastic and talented developer with a broad range of skills across the stack, from building out aesthetic and functional designs in Figma to the implementation of tickets. He approaches tech with an attention to detail, both visually and in terms of understanding product requirements. He has brought a great deal of value to his peers on the course, as well as to team mates in projects. I had the pleasure of supervising his final team project, and the determination and infectious positive spirit that he brought to proceeding was a huge part in the projects success. I’d have no hesitation to recommend Mo to any product driven engineering team that would benefit from his unique combination of skills and experience.“",
     avatar: "/images/rec-img/Alex_Withington-Smith.jpeg",
     name: "Alex Withington-Smith",
     title: "Software Engineer",
   },
   {
-    rating: 4,
-    review: "Review",
-    recommendation: "Great Quality!",
-    avatar: "",
-    name: "",
-    title: "",
+    rating: 5,
+    review: "Skillful Design and Technical Expertise",
+    recommendation:
+      "“With an exceptional eye for design and technical skills to bring products to life, Mo creates visually stunning, user-friendly digital experiences. His passion for responsive design results in immersive interfaces that engage audiences across devices. Mo is a multitalented professional who thrives on challenging projects and would be an invaluable asset to any team.”",
+    avatar: "/images/rec-img/Amy_Mead.jpeg",
+    name: "Amy Mead",
+    title: "Programme Manager",
   },
 ];
 
 export default recommendationsData;
 
 const rec2 = [
-  {
-    rating: 4,
-    review: "Review",
-    recommendation: "Great Quality!",
-    avatar: "/images/rec-img/Amy_Mead.jpeg",
-    name: "Amy Mead",
-    title: "Programme Manager",
-  },
   {
     rating: 4,
     review: "Review",
