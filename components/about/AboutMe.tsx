7ee9ce92518f5c7ba2bebfd4f328948dd329b884
Mo Mahboobian <momahboobian@gmail.com>
2023-11-20 23:03:02 +0000

    update on about me

---
+++ components/about/AboutMe.tsx
@@ -0,0 +1,105 @@
+export default function AboutMe() {
+  return (
+    <div>
+      <p className=" mt-4 placeholder:leading-relaxed">
+        &ldquo;I believe in the power of technology to transform ideas into
+        reality and enhance user experiences.&ldquo;
+      </p>
+      <p className="mt-4">
+        I began my journey as a web developer in 2015, and since then, I&#39;ve
+        continued to grow and evolve as a developer. Every new challenge and
+        technology I encounter becomes a stepping stone for my evolution as a
+        developer.
+      </p>
+      <p className="mt-4">
+        Specializing in web development has been an exhilarating ride.
+        Constantly enriching my skills and eagerly embracing the latest industry
+        trends have been the driving forces behind my progress, propelling me
+        towards meaningful achievements. Im always thrilled to dive into
+        innovative projects that create a positive impact.
+      </p>
+      <p className="mt-4">
+        Im an impassioned, self-proclaimed designer deeply rooted in full-stack
+        development. I enjoy tackling complex problems and crafting elegant
+        solutions that make a positive impact. I am very much a progressive way
+        to development. When Im not in full-on developer mode, you can find me
+        hovering around to learning more about the new technologies or on indie
+        hacker, or enjoying some free time.
+      </p>
+      <p className="mt-4">
+        My experience spans from creating user-friendly web interfaces to
+        optimizing backend systems. I thrive on challenges and am committed to
+        delivering top-notch solutions. Collaborating closely with clients,
+        I&#39;ve implemented features like patient data management, appointment
+        scheduling, and real-time chat, always ensuring the perfect fit for
+        their needs.
+      </p>
+      <p className="mt-4">
+        I&#39;m an enthusiastic self-proclaimed designer deeply rooted in
+        full-stack development. I revel in solving complex problems and crafting
+        elegant solutions that truly make a difference. Being a progressive
+        thinker, I enjoy seeing projects from ideation through to development.
+        And when I&#39;m not deep in coding, you&#39;ll find me exploring new
+        tech or on Indie Hacker, sometimes just taking a breather.
+      </p>
+      <p className="mt-4">
+        Curious to see some of my work? Check out my portfolio! And hey, if you
+        have a project in mind or want to team up, I&#39;m all ears for new
+        opportunities. Let&#39;s create something remarkable together!
+      </p>
+      <p className="mt-4">
+        Lastly, a few tidbits about me. Oh, and if you&#39;d like to chat, drop
+        me a line at info@moreel.me. I&#39;m always up for a good conversation!
+      </p>
+      {/* <div>
+        <h3 className="text-2xl font-semibold text-white mt-6 mb-3">
+          What I Do
+        </h3>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+          <div className="text-gray-500 leading-relaxed">
+            <h4 className="text-xl font-semibold text-yellow-400 mb-2">
+              Web Development
+            </h4>
+            <p>
+              As a developer, I find myself most captivated by the power and
+              flexibility of NEXT.js. I&#39;m always eager to dive into new
+              projects that leverage NEXT.js and discover innovative ways to
+              create fast, scalable, and user-friendly applications.
+            </p>
+          </div>
+          <div className="text-gray-500 leading-relaxed">
+            <h4 className="text-xl font-semibold text-yellow-400 mb-2">
+              App Development
+            </h4>
+            <p>
+              With a focus on user-centric design and cutting-edge technologies,
+              I thrive on building intuitive and efficient apps that make a
+              positive impact on people&&#39;s lives. Let&#39;s turn ideas into
+              reality and shape the future together.
+            </p>
+          </div>
+          <div className="text-gray-500 leading-relaxed">
+            <h4 className="text-xl font-semibold text-yellow-400 mb-2">
+              Multimedia Development
+            </h4>
+            <p>
+              Leveraging my background in video production, I enjoy exploring
+              multimedia development, crafting immersive experiences that blend
+              technology and visual storytelling.
+            </p>
+          </div>
+          <div className="text-gray-500 leading-relaxed">
+            <h4 className="text-xl font-semibold text-yellow-400 mb-2">
+              UI/UX Designing
+            </h4>
+            <p>
+              Crafting visually appealing and intuitive user interfaces that
+              offer a delightful user experience is something I&#39;m truly
+              fanatic about.
+            </p>
+          </div>
+        </div>
+      </div> */}
+    </div>
+  );
+}
