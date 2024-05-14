8005716aeee8f188af965ac50d79b9955a1e6e2c
Mo Mahboobian <momahboobian@gmail.com>
2024-05-14 11:09:43 +0100

    adding document file to for adscenc

---
+++ pages/_document.tsx
@@ -0,0 +1,21 @@
+import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <Script
+          async
+          src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_GOOGLE_ADS_CLIENT_ID}`}
+          strategy="lazyOnload"
+          crossOrigin="anonymous"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
