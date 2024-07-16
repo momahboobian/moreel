d77023c0c50914e564b0a020a9819d5511159cb8
Mo Mahboobian <momahboobian@gmail.com>
2024-07-16 19:02:54 +0100

    Updating next config to standalon

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  env: {
-    // @see https://github.com/facebookexperimental/Recoil/issues/2135#issuecomment-1362197710
-    RECOIL_DUPLICATE_ATOM_KEY_CHECKING_ENABLED: "false",
-  },
+  output: "standalone",
 };
 
 module.exports = nextConfig;
